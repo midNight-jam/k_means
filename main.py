25de66c7be5ae4e5cca46e568c52e12fc2da94a8
jayam <malviya.jayam@gmail.com>
2017-05-07 13:29:38 -0700

    normalized l2 : improved score

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import numpy as np
 from scipy.sparse import coo_matrix
 from scipy.sparse import csr_matrix
 from sklearn.metrics.pairwise import pairwise_distances
+import os.path
+from datetime import datetime
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import normalize
+
+
+def write_Cluster(cld, e):
+  subdir = 'results' + str(datetime.now()) + '_epcoh_{}_res'.format(e)
+  print(subdir)
+  try:
+    os.mkdir(subdir)
+  except Exception:
+    print(Exception)
+  f = open(os.path.join(subdir, 'KM_res_assig_{}.txt'.format(e)), 'w')
+  resD = {}
+  for k in cld:
+    for v in cld[k]:
+      resD[v] = k
+  for k in sorted(resD.keys()):
+    f.write('{}\n'.format(resD[k] + 1))
+  f.close()
 
 def readtrain_createcsr():
   # train_file = 'tiny.dat'
@@ -32,11 +53,10 @@ def readtrain_createcsr():
   print('Data Read Succesfully...')
   return coo_matrix((vals, (rows, cols)), shape=(row_id, max_feature )).tocsr()
 
-
-
-def K_Means(csr_data, K = 3, max_epoch = 10, max_iterations = 10):
+def K_Means_PairWise(csr_data, K = 3, max_epoch = 10, max_iterations = 10):
   print('Starting K Means')
   print('K : {}, max_Epoch : {}, max_Iterations : {} \n'.format(K, max_epoch, max_iterations))
+  # docs = 5
   docs = 8580
   cluster = {}
   f = open('res_K_Means.txt', 'w')
@@ -79,6 +99,7 @@ def K_Means(csr_data, K = 3, max_epoch = 10, max_iterations = 10):
     # print('\n~~~~ Epoch : {} Cluster : {}\n'.format(e, cluster))
     f.write('\n~~~~ Epoch : {} Cluster : {}\n\n'.format(e, cluster))
     write_Cluster(cluster, e)
+    calculate_SSE(cluster, csr_data, e)
 
     e += 1 # either we have reached the max_EPoCh
     if(e == max_epoch):
@@ -91,30 +112,104 @@ def K_Means(csr_data, K = 3, max_epoch = 10, max_iterations = 10):
   print('Sending to write assiginments')
   write_Cluster(cluster,e)
 
-import os.path
-from datetime import datetime
-def write_Cluster(cld, e):
-  subdir = 'results'+str(datetime.now()) + '_epcoh_{}_res'.format(e)
-  print(subdir)
+def K_Means_CosineSimilarity(csr_data, K = 3, max_epoch = 10, max_iterations = 10):
+  print('Starting K Means')
+  print('K : {}, max_Epoch : {}, max_Iterations : {} \n'.format(K, max_epoch, max_iterations))
+  # docs = 5
+  docs = 8580
+  cluster = {}
+  f = open('res_K_Means.txt', 'w')
+  f.write('K : {}, max_Epoch : {}, max_Iterations : {} \n'.format(K, max_epoch, max_iterations))
+  old_cluster = cluster
+  e = 1
+  while True:
+    print('{} Epoch : {} {} '.format('-'*10, e, '-'*10))
+    f.write('{} Epoch : {} {} \n'.format('-'*10, e, '-'*10))
+    centroid_indices = np.random.choice(docs, K, replace=False)
+    centroid_vectors = csr_data[centroid_indices].toarray()
+
+    # Loop Begins
+    for itr in range(max_iterations):
+      cluster = {}
+      for i in range(K):
+        cluster[i] = []
+
+      cv = csr_matrix(centroid_vectors)
+      cos_sim =  cosine_similarity(csr_data, cv, dense_output=False)# similarity between data & cluster centroids
+      # axis 1 for row, this is what we need to tell which row has shortest dist to which cluster
+      max_sim = cos_sim.argmax(axis=1).getA1()  # getA1() for flattening the matrix
+
+      for iv in range(max_sim.shape[0]):
+        cluster[max_sim[iv]].append(iv)# assiging the vect (rowID) to closest cluster
+
+      # readjusting the centroids by taking the mean
+      for i in range(K):
+        clust_vect_indices = cluster[i]
+        clust_vect_indices_data = csr_data[clust_vect_indices].toarray()
+        clust_mean = np.mean(clust_vect_indices_data, axis=0)
+        centroid_vectors[i] = clust_mean
+      print('Iteration : {}'.format(itr))
+
+
+      if(old_cluster == cluster): # no change detected
+        print('-----------NOCHNAGE termination EPoch--------')
+        break
+      old_cluster = cluster
+
+    print('Epoch {} completed '.format(e))
+    # print('\n~~~~ Epoch : {} Cluster : {}\n'.format(e, cluster))
+    # f.write('\n~~~~ Epoch : {} Cluster : {}\n\n'.format(e, cluster))
+    for k in sorted(cluster.keys()):
+      f.write('\ncluster : {} : size : {} \n'.format(k, len(cluster[k])))
+    write_Cluster(cluster, e)
+    calculate_SSE(cluster, csr_data, e)
+
+    e += 1 # either we have reached the max_EPoCh
+    if(e == max_epoch):
+      break
+      # Loop Ends
+
+  print('Fianl cluster assignments Writen')
+  f.write('Fianl cluster assignments : {}\n'.format(cluster))
+  f.close()
+  print('Sending to write assiginments')
+  write_Cluster(cluster,e)
+
+def calculate_SSE(cluster, csr_data, epoch):
+  print('-' * 50)
+  c_dist = 0
   try:
-    os.mkdir(subdir)
+    f = open('ssd_res_{}.txt'.format(epoch),'w')
+    f.write('{} Epcoch : {}\n'.format('-' * 20, epoch, '-' * 20))
+    for i in cluster:
+      cl = cluster[i]
+      clv = csr_data[cl].toarray()
+      center = np.mean(clv, 0)
+      center_sp = csr_matrix(center)
+      cl_data = csr_data[cl]
+      dist = pairwise_distances(cl_data, center_sp)  # dist between data & cluster centroids
+      dist_sum = np.sum(dist)
+      c_dist += dist_sum / len(cluster[i])
+      print('Cluster # : {}, Size :{},  Sum of Squared Distances : {}\n'.format(i, len(cluster[i]), dist_sum))
+      f.write('Cluster # : {}, Size :{},  Sum of Squared Distances : {}\n'.format(i, len(cluster[i]), dist_sum))
+
   except Exception:
-    print(Exception)
-  f = open(os.path.join(subdir, 'KM_res_assig_{}.txt'.format(e) ), 'w')
-  resD = {}
-  for k in cld:
-    for v in cld[k]:
-      resD[v] = k
-  for k in sorted(resD.keys()):
-    f.write('{}\n'.format(resD[k] + 1))
+    print('seom except')
+
+  print('Epoch # : {} , Sum of clusterSSD / clusterSize : {}\n'.format(epoch, c_dist))
+  print('-' * 50)
+  f.write('Epoch  # : {} , Sum of clusterSSD / clusterSize : {}\n'.format(epoch, c_dist))
   f.close()
 
 def main_two():
   train_csr = readtrain_createcsr()
+  train_csr = normalize(train_csr, norm='l2', axis=1)
+  # K = 3
   K = 7
   max_epoch = 10
   max_iterations = 100
-  K_Means(train_csr, K, max_epoch, max_iterations)
+  # K_Means_PairWise(train_csr, K, max_epoch, max_iterations)
+  K_Means_CosineSimilarity(train_csr, K, max_epoch, max_iterations)
 
 if __name__ == '__main__':
   main_two()
